33cabdd808c1f38f2476b9ebb6541cdda6f4e93d
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-07 12:49:07 +0200

    push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <h1>Todo App</h1>
-  <add-todo @add-todo="addTodo"></add-todo>
-  <display-todos :todos="todos" @delete-todo="deleteTodo"></display-todos>
+  <h1>Todo app</h1>
+  <form @submit.prevent="addNewTodo">
+    <h2>Add A New Todo</h2>
+    <input
+      v-if="state.todos.length"
+      type="search"
+      v-model="searchQuery"
+      placeholder="Search Todos"
+    />
+    <input type="text" v-model="state.newTodo" placeholder="add a new todo" />
+    <button>add</button>
+    <ul v-if="filteredTodos.length">
+      <li v-for="todo in filteredTodos" :key="todo.id">
+        <h3
+          @click="toggleDone(todo)"
+          :style="{
+            textDecoration: `${todo.done ? 'line-through' : 'none'}`,
+          }"
+        >
+          {{ todo.content }}
+        </h3>
+        <button @click="deleteTodo(todo.id)">Delete Todo</button>
+      </li>
+    </ul>
+    <h3 v-if="!filteredTodos.length">No todos</h3>
+  </form>
 </template>
 
-<script>
-import AddTodo from "./components/AddTodo";
-import displayTodos from "./components/displayTodos";
-export default {
-  name: "App",
-  components: { AddTodo, displayTodos },
-  data() {
-    return {
-      todos: [],
-    };
-  },
-  methods: {
-    addTodo(todo) {
-      this.todos.push(todo);
-    },
-    deleteTodo(index) {
-      this.todos = this.todos.filter((todo, i) => i !== index);
-    },
-  },
+<script setup>
+import { computed, reactive, ref } from 'vue';
+
+const state = reactive({
+  newTodo: '',
+  todos: [],
+});
+
+const searchQuery = ref('');
+
+const addNewTodo = () => {
+  if (state.newTodo) {
+    state.todos.push({
+      content: state.newTodo,
+      done: false,
+      id: Math.random(),
+    });
+  }
+  state.newTodo = '';
 };
+
+const toggleDone = (todo) => {
+  todo.done = !todo.done;
+};
+
+const deleteTodo = (id) => {
+  state.todos = state.todos.filter((todo) => todo.id !== id);
+};
+
+const filteredTodos = computed(() => {
+  if (searchQuery.value) {
+    return state.todos.filter((todo) =>
+      todo.content.toLowerCase().includes(searchQuery.value.toLowerCase())
+    );
+  } else {
+    return state.todos;
+  }
+});
 </script>
 
-<style>
-</style>
+<style></style>
